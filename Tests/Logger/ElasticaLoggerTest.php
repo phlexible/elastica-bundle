570e317eb0b9892946a3ebae5edf87823641420f
Stephan Wentz <swentz@brainbits.net>
2016-10-19 22:24:44 +0200

    fix docblocks, add README

--- Tests/Logger/ElasticaLoggerTest.php
+++ Tests/Logger/ElasticaLoggerTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the phlexible elastica package.
+ *
+ * (c) Stephan Wentz <sw@brainbits.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Phlexible\Bundle\ElasticaBundle\Tests\Logger;
 
 use Phlexible\Bundle\ElasticaBundle\Logger\ElasticaLogger;
